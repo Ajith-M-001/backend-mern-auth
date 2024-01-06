c0979665423f9a6b2d7dd17f54d596bcdd2ae974
Ajith-M-001 <ajithguru741@gmail.com>
2024-01-06 16:18:18 +0530

    07 User Registration: Validation and Password Hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,34 @@
 import asyncHandler from "express-async-handler";
+import userModel from "../model/userModel.js";
+import bcryptjs from "bcryptjs";
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "register user" });
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Required fields cannot be empty");
+  }
+
+  const userExists = await userModel.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email Already Exists! Please Login");
+  }
+  const hashedpassword = await bcryptjs.hash(password, 10);
+  const user = new userModel({
+    name,
+    email,
+    password: hashedpassword,
+  });
+
+  const newUser = await user.save();
+
+  if (newUser) {
+    res.status(201).json({ message: "User Registered Successfully" });
+  } else {
+    res.status(400);
+    throw new Error("Unable to register user");
+  }
 });
 
 export const loginUser = asyncHandler(async (req, res) => {
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/users", userRouter);
 app.get("/", (req, res) => {
   res.send("Server started");
