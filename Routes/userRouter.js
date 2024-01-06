551f2f70971ed60f0d61a62bdac59f71d4f88efa
Ajith-M-001 <ajithguru741@gmail.com>
2024-01-06 15:47:06 +0530

    Define other routes for user actions (login, logout, update, delete)

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import {
+  deleteUser,
+  loginUser,
+  logoutUser,
+  registerUser,
+  updateUser,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
-
+userRouter.post("/login", loginUser);
+userRouter.post("/logout", logoutUser);
+userRouter.put("/update", updateUser);
+userRouter.delete("/delete", deleteUser);
 export default userRouter;
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,3 +3,19 @@ import asyncHandler from "express-async-handler";
 export const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "register user" });
 });
+
+export const loginUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "login user" });
+});
+
+export const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "logout user" });
+});
+
+export const updateUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "update user" });
+});
+
+export const deleteUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "delete user" });
+});
